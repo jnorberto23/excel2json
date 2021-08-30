4e6feb14c82c38f252cf0466547fab72993b1627
jnorberto23 <joaovictor.norberto@outlook.com>
2021-08-30 13:06:52 -0300

    deploy settings

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://excel2json.vercel.app/",
+  baseURL: "https://excel2json-server.herokuapp.com/",
 });
 
 export default api;
