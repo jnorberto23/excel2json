afc16028ce2a0af4c98f454772cba86cac4a2bc8
jnorberto23 <joaovictor.norberto@outlook.com>
2021-08-30 16:07:42 -0300

    add href to about link

--- app/src/components/Header/index.js
+++ app/src/components/Header/index.js
@@ -7,7 +7,7 @@ const Header = () =>{
         <Container>
           <Navbar.Brand href="#home">excel2json</Navbar.Brand>
             <Nav className="">
-              <Nav.Link href="https://github.com/jnorberto23/excel2json">About</Nav.Link>
+              <Nav.Link href="https://github.com/jnorberto23/excel2json" target="_blank">About</Nav.Link>
             </Nav>
         </Container>
       </Navbar>
