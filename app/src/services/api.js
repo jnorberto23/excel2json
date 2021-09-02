a158a1bd90b71d594bc691d493bb85354eda9d7b
jnorberto23 <joaovictor.norberto@outlook.com>
2021-09-01 21:00:29 -0300

    change api address

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://excel2json-server.herokuapp.com/",
+  baseURL: "https://excel2json-server.vercel.app/",
 });
 
 export default api;
